fd148d19af5dec2fd84dab520498425d89217df5
johandquintero@utp.edu.co <johandquintero@utp.edu.co>
2023-09-04 17:19:47 -0500

    run on chromium only

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    /*
     
     {
       name: 'firefox',
@@ -46,6 +47,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    */
 
     /* Test against mobile viewports. */
     // {
