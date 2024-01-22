d71863f86d277a971e26db1b7ef94aeb8e98e7a4
Johany Quintero <johandquintero@utp.edu.co>
2024-01-22 18:57:58 -0500

    Improve steps

--- api/tests/acceptance/acceptance_test.spec.ts
+++ api/tests/acceptance/acceptance_test.spec.ts
@@ -2,11 +2,15 @@ import { test, expect } from '@playwright/test';
 import { CatFact } from '../../types/CatFact';
 import { CatFactController } from '../../controllers/CatFactController';
 
-test('should get cats', async ({ request }) => {
+test('Cat Fact Service', async ({ request }) => {
+  let catFacts: CatFact;
+
   await test.step('Given user GET cat facts', async () => {
     const catFactController = new CatFactController(request);
-    const catFacts: CatFact = await catFactController.getCatFact();
+    catFacts = await catFactController.getCatFact();
+  });
 
+  await test.step('Then cat fact response should contain correct properties', async () => {
     expect(catFacts).toHaveProperty('fact');
     expect(catFacts).toHaveProperty('length');
     expect(catFacts).not.toBe(null);
