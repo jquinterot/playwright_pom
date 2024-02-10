fee2c51942df0f901b7deaa06fdc62e13ebff7f3
Johany Quintero <johandquintero@utp.edu.co>
2024-02-10 14:07:06 -0500

    distribute actions instances

--- ui/tests/order_management/check_add_to_cart.spec.ts
+++ ui/tests/order_management/check_add_to_cart.spec.ts
@@ -3,17 +3,14 @@ import { Products, ProductsPrices } from '../../helpers/enums/Products';
 import { MenuOptions } from '../../helpers/enums/MenuOptions';
 
 test.describe('@regression Check that a product can be added to the cart', () => {
-
   test.beforeEach(async ({ page }) => {
     await page.goto('');
   });
 
   test('Check that a samsung cellphone can be added', async ({
-    actionFactory
+    actionFactory,
   }) => {
     const homeActions = actionFactory.createHomeActions();
-    const productActions = actionFactory.createProductActions();
-    const cartActions = actionFactory.createCartActions();
 
     await test.step('When selects Phone category', async () => {
       await homeActions.selectCategory('Phones');
@@ -22,6 +19,8 @@ test.describe('@regression Check that a product can be added to the cart', () =>
     await test.step('And selects Samsung Galaxy S6 product', async () => {
       await homeActions.selectProduct(Products.GALAXY_S6);
     });
+
+    const productActions = actionFactory.createProductActions();
 
     await test.step('Then Samsung Galaxy S6 is displayed in product summary page', async () => {
       await productActions.checkAddedProduct(Products.GALAXY_S6);
@@ -35,6 +34,8 @@ test.describe('@regression Check that a product can be added to the cart', () =>
     await test.step('And goes to cart', async () => {
       await homeActions.selectMenuOption(MenuOptions.CART);
     });
+
+    const cartActions = actionFactory.createCartActions();
 
     await test.step('Then the Samsung Galaxy S6 is added to cart', async () => {
       await cartActions.checkProductIsDisplayed(Products.GALAXY_S6);
--- ui/tests/order_management/check_place_order.spec.ts
+++ ui/tests/order_management/check_place_order.spec.ts
@@ -5,67 +5,69 @@ import { johnCardInfo } from '../../helpers/objects/CardInfo';
 import { johnInfo } from '../../helpers/objects/CustomerInfo';
 
 test.describe('@regression Check place order', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');
+  });
 
-    test.beforeEach(async ({ page }) => {
-      await page.goto('');
+  test('Check that a samsung cellphone can be ordered', async ({
+    actionFactory,
+  }) => {
+    const homeActions = actionFactory.createHomeActions();
+
+    await test.step('When selects Phone category', async () => {
+      await homeActions.selectCategory('Phones');
     });
-  
-    test('Check that a samsung cellphone can be ordered', async ({
-      actionFactory
-    }) => {
-      const homeActions = actionFactory.createHomeActions();
-      const productActions = actionFactory.createProductActions();
-      const cartActions = actionFactory.createCartActions();
-      const placeOrderActions = actionFactory.createPlaceOrderActions();
-  
-      await test.step('When selects Phone category', async () => {
-        await homeActions.selectCategory('Phones');
-      });
-  
-      await test.step('And selects Samsung Galaxy S6 product', async () => {
-        await homeActions.selectProduct(Products.GALAXY_S6);
-      });
-  
-      await test.step('Then Samsung Galaxy S6 is displayed in product summary page', async () => {
-        await productActions.checkAddedProduct(Products.GALAXY_S6);
-        await productActions.checkProductPrice(ProductsPrices.GALAXY_S6_PRICE);
-      });
-  
-      await test.step('When adds the Samsung Galaxy S6 to the cart', async () => {
-        await productActions.addToCart();
-      });
-  
-      await test.step('And goes to cart', async () => {
-        await homeActions.selectMenuOption(MenuOptions.CART);
-      });
-  
-      await test.step('Then the Samsung Galaxy S6 is added to cart', async () => {
-        await cartActions.checkProductIsDisplayed(Products.GALAXY_S6);
-      });
-  
-      await test.step('And the Samsung Galaxy S6product is deleted', async () => {
-        await cartActions.deleteProductFromCard(Products.GALAXY_S6);
-      });
 
-      await test.step('When selects place order', async () => {
-        await cartActions.selectPlaceOrder();
-      });
+    await test.step('And selects Samsung Galaxy S6 product', async () => {
+      await homeActions.selectProduct(Products.GALAXY_S6);
+    });
 
-      await test.step('And fills place order form', async () => {
-        await placeOrderActions.fillName(johnInfo.name);
-        await placeOrderActions.fillCountry(johnInfo.country);
-        await placeOrderActions.fillCity(johnInfo.city);
-        await placeOrderActions.fillCard(johnCardInfo.number);
-        await placeOrderActions.fillMonth(johnCardInfo.month);
-        await placeOrderActions.fillYear(johnCardInfo.year);
-      });
+    const productActions = actionFactory.createProductActions();
 
-      await test.step('And selects purchase', async () => {
-        await placeOrderActions.selectPurchase();
-      });
+    await test.step('Then Samsung Galaxy S6 is displayed in product summary page', async () => {
+      await productActions.checkAddedProduct(Products.GALAXY_S6);
+      await productActions.checkProductPrice(ProductsPrices.GALAXY_S6_PRICE);
+    });
 
-      await test.step('Then thank you message is displayed', async () => {
-        await placeOrderActions.isThankYouMessageIsDisplayed();
-      });
+    await test.step('When adds the Samsung Galaxy S6 to the cart', async () => {
+      await productActions.addToCart();
+    });
+
+    await test.step('And goes to cart', async () => {
+      await homeActions.selectMenuOption(MenuOptions.CART);
+    });
+
+    const cartActions = actionFactory.createCartActions();
+
+    await test.step('Then the Samsung Galaxy S6 is added to cart', async () => {
+      await cartActions.checkProductIsDisplayed(Products.GALAXY_S6);
+    });
+
+    await test.step('And the Samsung Galaxy S6product is deleted', async () => {
+      await cartActions.deleteProductFromCard(Products.GALAXY_S6);
+    });
+
+    await test.step('When selects place order', async () => {
+      await cartActions.selectPlaceOrder();
+    });
+
+    const placeOrderActions = actionFactory.createPlaceOrderActions();
+
+    await test.step('And fills place order form', async () => {
+      await placeOrderActions.fillName(johnInfo.name);
+      await placeOrderActions.fillCountry(johnInfo.country);
+      await placeOrderActions.fillCity(johnInfo.city);
+      await placeOrderActions.fillCard(johnCardInfo.number);
+      await placeOrderActions.fillMonth(johnCardInfo.month);
+      await placeOrderActions.fillYear(johnCardInfo.year);
+    });
+
+    await test.step('And selects purchase', async () => {
+      await placeOrderActions.selectPurchase();
+    });
+
+    await test.step('Then thank you message is displayed', async () => {
+      await placeOrderActions.isThankYouMessageIsDisplayed();
     });
   });
+});
